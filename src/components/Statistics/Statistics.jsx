7b920cbdec26fa51520d44c73fb4b6c6f9327f53
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-02-15 11:24:23 +0100

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,26 +1,25 @@
 import PropTypes from "prop-types";
 import styles from "./Statistics.module.css";
 
-const Statistics = ({ state, total, positivePercentage }) => {
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
     return (
         <>
-            {Object.entries(state).map(([key, value]) => (
-                <p className={styles.text} key={key}>
-                    {key}:{value}
-                </p>
-            ))}
-            <p className={styles.text}>total: {total}</p>
+            <ul className={styles.list}>
+                <li className={styles.text}>Good: {good}</li>
+                <li className={styles.text}>Neutral: {neutral}</li>
+                <li className={styles.text}>Bad: {bad}</li>
+            </ul>
+
+            <p className={styles.text}>Total: {total}</p>
             <p className={styles.text}>Positive feedback: {positivePercentage}%</p>
         </>
     );
 };
 
 Statistics.propTypes = {
-    state: PropTypes.shape({
-        good: PropTypes.number.isRequired,
-        neutral: PropTypes.number.isRequired,
-        bad: PropTypes.number.isRequired,
-    }).isRequired,
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
     total: PropTypes.number.isRequired,
     positivePercentage: PropTypes.number.isRequired,
 };
